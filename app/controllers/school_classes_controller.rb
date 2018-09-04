9bfff72ef3f85c148381fca8fa56b693553b37f0
Jonathan Hong <jonhong777@gmail.com>
2018-09-04 19:57:47 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,7 +5,7 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.create(params)
-    redirect_to
+    redirect_to school_class_path(@school_class)
   end
 
   def show
